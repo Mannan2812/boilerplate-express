46ac128896bd3f7f9f3849ed737a08432ae23027
Mannan bansal <t-mbansal@microsoft.com>
2021-07-01 00:02:26 +0530

    Added static files in express

--- myApp.js
+++ myApp.js
@@ -3,6 +3,11 @@ var app = express();
 
 console.log("Hello World")
 
+staticPath = __dirname + "/public"
+
+app.use("/public",express.static(staticPath))
+
+
 app.get('/', (req, res) => {
     // res.send("Hello Express")
     absolutePath = __dirname + "/views/index.html"
